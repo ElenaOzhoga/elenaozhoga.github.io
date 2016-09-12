4436e6678219c1f06bed5ef126700a0a450bed52
Elena Ozhoga <helen.ozhogamaslovsk@gmail.com>
2016-09-12 14:33:26 +0300

    modified js with adding functions

--- markupJavascript/js/main.js
+++ markupJavascript/js/main.js
@@ -1,53 +1,65 @@
+function displayDomContent (id, contentVar) {
+	document.getElementById(id).innerHTML = contentVar;
+}
+
+function buildHTML (questions, labelVal, nameVal, requiredVal, fieldVal, optionsVal, contentVar) {
+	if (questions !== undefined) {
+		contentVar += '<div class="field-row">';
+		if (labelVal !== undefined) {
+			contentVar += '<div class="label-text"><label' + (nameVal ? ' for="' + nameVal + '"' : '') + '>' + labelVal + '</label>';
+			if (requiredVal === true) {
+				contentVar += '<span class="required">*</span>';
+			}
+			contentVar += '</div>';
+		}
+		if (fieldVal !== undefined) {
+			if (fieldVal === "Text") {
+				contentVar += '<div class="field-control"><input type="text" class="text-input"' + (nameVal ? ' id="' + nameVal + '"' : '') + (nameVal ? ' name="' + nameVal + '"' : '') + '/></div>';
+			}
+			if (fieldVal === "Select") {
+				contentVar += '<div class="field-control"><select' + (nameVal ? ' id="' + nameVal + '"' : '') + (nameVal ? ' name="' + nameVal + '"' : '') + '><option value="">-please select-</option>';
+				if (optionsVal !== undefined) {
+					for (var j = 0; j < optionsVal.length; j++) {
+						contentVar += '<option value="' + optionsVal[j].value + '">' + optionsVal[j].label + '</option>';
+					}
+				}
+				contentVar += '</select></div>';
+			}
+			if (fieldVal === "Radio") {
+				contentVar += '<div class="col">';
+				if (optionsVal !== undefined) {
+					for (var j = 0; j < optionsVal.length; j++) {
+						contentVar += '<div class="field-item"><input' + (nameVal ? ' id="' + nameVal + j + '"' : '') + (nameVal ? ' name="' + nameVal + '"' : '') + ' type="radio" value ="' + optionsVal[j].value +'" class="radio"/><label' + (nameVal ? ' for="' + nameVal + j + '"' : '') + 'class="field-label">' + optionsVal[j].label + '</label></div>';
+					}
+				}
+				contentVar += '</div>';
+			}
+			if (fieldVal === "Textarea") {
+				contentVar += '<div class="field-control"><textarea cols="3" rows="3"' + (nameVal ? ' id="' + nameVal + '"' : '') + (nameVal ? ' name="' + nameVal + '"' : '') + '></textarea></div>';
+			}
+		}
+		contentVar += '</div>';
+		return contentVar;
+	}
+
+}
+
+function getQuestions (dataForm) {
+	var domContent = '';
+	domContent += '<form class="application-form" action="#"><fieldset>';
+	if (dataForm.questions !== undefined) {
+		for (var i = 0; i < dataForm.questions.length; i++) {
+			domContent = buildHTML (dataForm.questions, dataForm.questions[i].label, dataForm.questions[i].name, dataForm.questions[i].required,
+					dataForm.questions[i].field_type, dataForm.questions[i].options, domContent);
+		}
+		domContent += '<div class="btn-block"><input type="submit" value="Submit" class="btn-submit"/></div></fieldset></form>';
+		displayDomContent ('content', domContent);
+	}
+}
+
 (function () {
 	var formContainer = restAPI.form;
-	var domContent = '';
 	if (formContainer !== undefined) {
-		domContent += '<form class="application-form" action="#"><fieldset>';
-		if (formContainer.questions !== undefined) {
-			for (var i = 0; i < restAPI.form.questions.length; i++) {
-				domContent += '<div class="field-row">';
-				var fieldType = formContainer.questions[i].field_type;
-				var labelValue = formContainer.questions[i].label;
-				var nameValue = formContainer.questions[i].name;
-				var optionsValue = formContainer.questions[i].options;
-
-				if (labelValue !== undefined) {
-					domContent += '<div class="label-text"><label' + (nameValue ? ' for="' + nameValue + '"' : '') + '>' + labelValue + '</label>';
-					if (formContainer.questions[i].required === true) {
-						domContent += '<span class="required">*</span>';
-					}
-					domContent += '</div>';
-				}
-				if (fieldType !== undefined) {
-					if (fieldType === "Text") {
-						domContent += '<div class="field-control"><input type="text" class="text-input"' + (nameValue ? ' id="' + nameValue + '"' : '') + (nameValue ? ' name="' + nameValue + '"' : '') + '/></div>';
-					}
-					if (fieldType === "Select") {
-						domContent += '<div class="field-control"><select' + (nameValue ? ' id="' + nameValue + '"' : '') + (nameValue ? ' name="' + nameValue + '"' : '') + '><option value="">-please select-</option>';
-						if (optionsValue !== undefined) {
-							for (var j = 0; j < optionsValue.length; j++) {
-								domContent += '<option value="' + optionsValue[j].value + '">' + optionsValue[j].label + '</option>';
-							}
-						}
-						domContent += '</select></div>';
-					}
-					if (fieldType === "Radio") {
-						domContent += '<div class="col">';
-						if (optionsValue !== undefined) {
-							for (var j = 0; j < optionsValue.length; j++) {
-								domContent += '<div class="field-item"><input' + (nameValue ? ' id="' + nameValue + j + '"' : '') + (nameValue ? ' name="' + nameValue + '"' : '') + ' type="radio" value ="' + optionsValue[j].value +'" class="radio"/><label' + (nameValue ? ' for="' + nameValue + j + '"' : '') + 'class="field-label">' + optionsValue[j].label + '</label></div>';
-							}
-						}
-						domContent += '</div>';
-					}
-					if (fieldType === "Textarea") {
-						domContent += '<div class="field-control"><textarea cols="3" rows="3"' + (nameValue ? ' id="' + nameValue + '"' : '') + (nameValue ? ' name="' + nameValue + '"' : '') + '></textarea></div>';
-					}
-				}
-				domContent += '</div>';
-			}
-			domContent += '<div class="btn-block"><input type="submit" value="Submit" class="btn-submit"/></div></fieldset></form>';
-			document.getElementById('content').innerHTML += domContent;
-		}
+		getQuestions (formContainer);
 	}
 })();
